c57133cd8cfd74d4b348f777ff12b1d4c2cf8a15
Ketevan Laperishvili <klaperishvili@credo.ge>
2024-10-26 20:43:46 +0400

    Task3

---
+++ src/test/java/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateArea(5.5, 4.0));
+    }
+}
